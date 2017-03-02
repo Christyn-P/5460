a67bd5999b5d9479b416ec6a013134e0523e333d
= <naniganz@gmail.com>
2017-03-01 17:59:54 -0700

    adding the files now

---
+++ proj3/.hello.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/student/cphillippi/5460/proj3/hello.ko := ld -r -m elf_x86_64 -T /usr/src/linux-headers-3.13.0-45-generic/scripts/module-common.lds --build-id  -o /home/student/cphillippi/5460/proj3/hello.ko /home/student/cphillippi/5460/proj3/hello.o /home/student/cphillippi/5460/proj3/hello.mod.o
---
+++ proj3/.tmp_versions/hello.mod
@@ -0,0 +1,2 @@
+/home/student/cphillippi/5460/proj3/hello.ko
+/home/student/cphillippi/5460/proj3/hello.o
